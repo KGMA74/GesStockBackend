0ee8a18ce8fe7b5a15bf93f320c1f8d7d3b6eb12
KGMA74 <marmelkambou2@gmail.com>
2025-08-27 12:53:49 +0000

    checkpoint

--- GesStockBackend/settings.py
+++ GesStockBackend/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'corsheaders',
     'djoser',
+    'compressor',
 
     
     # project applications
@@ -121,6 +122,9 @@ STATICFILES_DIRS = [
 
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+COMPRESS_ENABLED = True
+COMPRESS_OFFLINE = True  # Active la compression hors ligne
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
